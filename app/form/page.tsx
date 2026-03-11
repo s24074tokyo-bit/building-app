8cf0121da64a32fcde05f31cefdefa1480f81942
Daichi Sato <s24074tokyo@gmail.com>
2026-03-12 02:34:07 +0900

    fix prerender form page

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic"
+
 import React, { useState, useEffect, Suspense } from "react";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db, auth } from "@/lib/firebase";
